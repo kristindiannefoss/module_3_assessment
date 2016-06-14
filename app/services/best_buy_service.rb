c8f18564aeb5481067e6390f7c45853042cc552d
Kris Foss <kris.foss@gmail.com>
2016-06-14 11:22:54 -0600

    still working on the connection syntax

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -9,10 +9,7 @@ class BestBuyService
   def stores(zip)
     #  "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&apiKey=#{ENV[best_buy_secret_key]}"
 
-    conn.get "/stores(area(#{zip},25))", {
-      apiKey: ENV["best_buy_secret_key"],
-      format: "json"
-    }
+    conn.get "stores(area(#{zip},25))?format=json&apiKey=#{ENV[best_buy_secret_key]}"
   end
 
 end
