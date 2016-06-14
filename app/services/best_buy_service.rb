eba11138e45ea5067d87327a23836426fd8f779d
Kris Foss <kris.foss@gmail.com>
2016-06-14 10:43:47 -0600

    adding a service for the bb api

---
+++ app/services/best_buy_service.rb
@@ -0,0 +1,4 @@
+class BestBuyService
+  
+
+end
--- spec/features/user_visits_home_pages_spec.rb
+++ spec/features/user_visits_home_pages_spec.rb
@@ -6,18 +6,18 @@ RSpec.feature "UserVisitsHomePages", type: :feature do
     # When I visit "/"
     visit "/"
     # And I fill in a search box with "80202"
-    fill_in "", with: "80202"
+    fill_in "q", with: "80202"
     #  and click "search"
     click_on "search"
     # Then my current path should be "/search" (ignoring params)
-    expect(current_path)
-
+    expect(current_path).to eq("/search")
+    # And I should see stores within 25 miles of 80202
+    # expect()
 
   end
 
 
 
-  # And I should see stores within 25 miles of 80202
   # And I should see a message that says "17 Total Stores"
   # And I should see exactly 15 results
   # And I should see the long name, city, distance, phone number and store type for each of the 15 results
