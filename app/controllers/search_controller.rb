78a75abfefcfa64f3eb991804d7c2de83eac9e93
Kris Foss <kris.foss@gmail.com>
2016-06-14 10:48:12 -0600

    setting up the connection in the service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
-  
+
+  def index
+    @stores = Stores.all
+  end
 
 end
--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,4 +1,15 @@
 class BestBuyService
-  
+
+  attr_reader :conn
+
+  def initialize
+    @conn = Faraday.new(url: 'https://api.bestbuy.com/v1')
+  end
+
+  def stores(data)
+    conn.get "/stores", {
+      apiKey: ENV[""]
+    }
+  end
 
 end
